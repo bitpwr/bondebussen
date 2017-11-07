ae87970ca53dc78eb4f4accb19a405fb3b589b7e
PM Holtmo <pm@holtmo.se>
2017-11-07 23:08:14 +0100

    Sort buses by stop point and show list of destinations per stop

--- app.js
+++ app.js
@@ -41,7 +41,12 @@ var requestOpts = {
     qs: {
         key: conf.key,
         siteid: 5812,
-        timewindow: 60
+        timewindow: 60,
+        bus: true,
+        metro: false,
+        train: false,
+        tram: false,
+        ship: false
     },
     json: true,
     maxAttempts: 3,
@@ -54,11 +59,77 @@ function timeformat(date) {
     return z(date.getHours()) + ':' + z(date.getMinutes());
 }
 
+function convertSlRealtime(data) {
+    var out = { title: "Bondebussen | Busstider i Järfälla",
+                gtag: conf.gtag };
+
+    var date = new Date(data.ResponseData.LatestUpdate);
+    out.checktime = date.toLocaleTimeString();
+
+    out.stops = [];
+
+    var buses = data.ResponseData.Buses
+    for (var i=0; i < buses.length; ++i)
+    {
+        var date1 = new Date(buses[i].TimeTabledDateTime);
+        var date2 = new Date(buses[i].ExpectedDateTime);
+
+        var time1 = timeformat(date1);
+        var time2 = timeformat(date2);
+
+        var bus = { number: buses[i].LineNumber, destination: buses[i].Destination,
+             time: time1, timeExpected: time2 };
+
+        if (buses[i].DisplayTime != time2) {
+            bus.display = buses[i].DisplayTime;
+        }
+
+        if (time1 != time2) {
+            bus.delayed = true
+        }
+
+        var stop = buses[i].StopPointNumber;
+
+        var found = false;
+        for (var j = 0; j < out.stops.length; ++j) {
+            var s = out.stops[j];
+            if (s.stop == stop) {
+                found = true;
+                s.buses.push(bus);
+                // check if destination exists
+                var destFound = false;
+                for (var x = 0; x < s.destinations.length; ++x) {
+                    if (s.destinations[x] == bus.destination) {
+                        destFound = true;
+                        break;
+                    }
+                }
+                if (!destFound) {
+                    s.destinations.push(bus.destination);
+                }
+                break;
+            }
+        }
+
+        if (!found) {
+            var s = {stop: stop, destinations: [bus.destination], buses: []};
+            s.buses.push(bus);
+            out.stops.push(s);
+            
+            if (!out.name) {
+                out.name = buses[i].StopAreaName
+            }
+        }
+    }
+
+    return out;
+}
+
 app.get('/', function (req, res) {
-    request(requestOpts, function(err, response, body) {
+    requestOpts.qs.siteid = 5812;
+    request(requestOpts, function(err, response, data) {
         if (!err && response.statusCode == 200) {
             // we get a json object directly
-            var data = body;
 
             if (data.StatusCode != 0)
             {
@@ -67,42 +138,14 @@ app.get('/', function (req, res) {
                 return;
             }
 
-            var date = new Date(data.ResponseData.LatestUpdate);
-            var str = ''
-
-            var buses = data.ResponseData.Buses
-            var busList1 = []
-            var busList2 = []
-            for (var i=0; i < buses.length; ++i)
-            {
-                var date1 = new Date(buses[i].TimeTabledDateTime);
-                var date2 = new Date(buses[i].ExpectedDateTime);
-
-                var time1 = timeformat(date1);
-                var time2 = timeformat(date2);
-
-                var bus = { number: buses[i].LineNumber, destination: buses[i].Destination,
-                     time: time1, timeExpected: time2 };
-                
-                if (buses[i].DisplayTime != time2) {
-                    bus.display = buses[i].DisplayTime;
-                }
-                
-                if (time1 != time2) {
-                    bus.delayed = true
-                }
-
-                if (buses[i].JourneyDirection == 1) {
-                    busList1.push(bus)
-                }
-                else {
-                    busList2.push(bus)
-                }
+            out = convertSlRealtime(data);
+            
+            if (out.stops.length == 0) {
+                res.render('error', {message: 'Det går inga bussar den närmsta timmen eller så finns inte hållplatsen'});
             }
-            res.render('busses', { title: "Bondebussen | Busstider vid Bondevägen, Järfälla",
-                checktime: date.toLocaleTimeString(),
-                gtag: conf.gtag,
-                buses1: busList1, buses2:busList2})
+            else {
+                res.render('buses2', out)
+            }
         }
         else {
             console.log("Error: " + err.message);
@@ -112,6 +155,19 @@ app.get('/', function (req, res) {
             res.render('error', { message: 'Sorry, no response from SL. Please refresh your page and try again.' });
         }
     })
+})
+
+app.get('/stop/:id', function(req, res) {
+    console.log("Get busses at stop " + req.params.id)
+    opts = requestOpts;
+    opts.qs.siteid = req.params.id;
+    request(requestOpts, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            // check sl error
+            out = convertSlRealtime(body);
+            res.render('buses2', out)
+        }
+    });
 })
 
 app.get('/about', function (req, res) {
