7323f63fa97f9a55f45519fe4a8874b60daacabc
PM Holtmo <pm.holtmo@gmail.com>
2019-01-15 22:33:20 +0100

    Selection of transport type and showing icons

--- webclient/src/components/Departures.vue
+++ webclient/src/components/Departures.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class>
-    <h4>{{destinationText}}</h4>
-    <div class="list-group list-group-flush">
-      <div v-for="dep in departures">
-        <div class="list-group-item list-group-item-action">
-          <span v-if="showNumber">{{ dep.number }} </span>
-          <span>{{ dep.destination }} - {{ dep.time }}</span>
+  <div class="card">
+    <div class="card-header">Mot {{destinationText}}</div>
+    <!-- <div class="card-body"> -->
+      <ul class="list-group list-group-flush">
+        <div v-for="dep in departures">
+          <li class="list-group-item list-group-item-action border-bottom">
+            <i class="fa" v-bind:class="[faIcon]" area-hidden=true></i>
+            <span v-if="showNumber"> {{ dep.number }}</span>
+            <span> {{ dep.destination }} - {{ dep.time }}</span>
+            <span v-if="display" class="">({{display}})</span>
+          </li>
         </div>
-      </div>
-    </div>
+      </ul>
+    <!-- </div> -->
   </div>
 </template>
 
@@ -17,7 +21,9 @@ export default {
   props: {
     destinationText: String,
     departures: Array,
-    showNumber: Boolean
+    showNumber: Boolean,
+    faIcon: String,
+    display: String
   }
 };
 </script>
--- webclient/src/views/Home.vue
+++ webclient/src/views/Home.vue
@@ -3,36 +3,60 @@
     <button class="btn btn-primary" v-on:click="get">Update</button>
     <input v-model="stop"/>
 
-    <div v-if="info.busStops && info.busStops.length > 0">
-      <h3>Bussar från {{ info.busStation }}</h3>
-      <div class="" v-for="stop in info.busStops">
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <button v-if="gotBusses() && typeCount() > 1" class="nav-link btn" v-bind:class="{ active: type == 0 }"
+                v-on:click="type = 0">Buss</button>
+      </li>
+      <li class="nav-item">
+        <button v-if="gotTrains() && typeCount() > 1" class="nav-link btn" v-bind:class="{ active: type == 1 }"
+                v-on:click="type = 1">Pendeltåg</button>
+      </li>
+      <li class="nav-item">
+        <button v-if="gotMetros() && typeCount() > 1" class="nav-link btn" v-bind:class="{ active: type == 2 }"
+                v-on:click="type = 2">Tunnelbana</button>
+      </li>
+      <li class="nav-item">
+        <button v-if="gotTrams() && typeCount() > 1" class="nav-link btn" v-bind:class="{ active: type == 3 }"
+                v-on:click="type = 3">Tvärbana</button>
+      </li>
+    </ul>
+
+    <div v-if="type == 0 && info.busStops && info.busStops.length > 0">
+      <h3>Bussar från {{ info.busStation }}<div class="float-right">{{ info.checktime }}</div></h3>
+      <div v-for="stop in info.busStops">
         <Departures v-bind:destinationText="stop.destinationText"
                     v-bind:departures=stop.departures
-                    showNumber />
+                    showNumber
+                    faIcon="fa-bus text-danger"
+                    v-bind:display="stop.display" />
       </div>
     </div>
 
-    <div v-if="info.trainStops && info.trainStops.length > 0">
-      <h3>Pendeltåg från {{ info.trainStation }}</h3>
+    <div v-if="type == 1 && info.trainStops && info.trainStops.length > 0">
+      <h3>Pendeltåg från {{ info.trainStation }}<div class="float-right">{{ info.checktime }}</div></h3>
       <div class="" v-for="stop in info.trainStops">
         <Departures v-bind:destinationText="stop.destinationText"
-                    v-bind:departures=stop.departures />
+                    v-bind:departures=stop.departures
+                    faIcon="fa-train text-primary" />
       </div>
     </div>
 
-    <div v-if="info.metroStops && info.metroStops.length > 0">
-      <h3>Tunnelbana från {{ info.metroStation }}</h3>
+    <div v-if="type == 2 && info.metroStops && info.metroStops.length > 0">
+      <h3>Tunnelbana från {{ info.metroStation }}<div class="float-right">{{ info.checktime }}</div></h3>
       <div class="" v-for="stop in info.metroStops">
         <Departures v-bind:destinationText="stop.destinationText"
-                    v-bind:departures=stop.departures />
+                    v-bind:departures=stop.departures
+                    faIcon="fa-subway text-success" />
       </div>
     </div>
 
-    <div v-if="info.tramStops && info.tramStops.length > 0">
-      <h3>Tvärbana från {{ info.tramStation }}</h3>
+    <div v-if="type == 3 && info.tramStops && info.tramStops.length > 0">
+      <h3>Tvärbana från {{ info.tramStation }}<div class="float-right">{{ info.checktime }}</div></h3>
       <div class="" v-for="stop in info.tramStops">
         <Departures v-bind:destinationText="stop.destinationText"
-                    v-bind:departures=stop.departures />
+                    v-bind:departures=stop.departures
+                    faIcon="fa-car text-secondary" />
       </div>
     </div>
 
@@ -55,6 +79,13 @@ function toJson(response) {
   return response.json();
 }
 
+// enum DepType {
+//   Bus,
+//   Train,
+//   Metro,
+//   Tram
+// };
+
 export default {
   name: 'home',
   components: {
@@ -63,7 +94,8 @@ export default {
   data() {
       return {
           stop: 5812,
-          info: {}
+          info: {},
+          type: 0
       }
   },
   methods: {
@@ -73,8 +105,27 @@ export default {
       then(checkStatus).then(toJson).
       then(res => { self.info = res; }).
       catch(err => console.log("Error: " + err))
+    },
+    gotBusses: function () {
+      return (this.info.busStops && this.info.busStops.length > 0)
+    },
+    gotTrains: function () {
+      return this.info.trainStops && this.info.trainStops.length > 0
+    },
+    gotMetros: function () {
+      return this.info.metroStops && this.info.metroStops.length > 0
+    },
+    gotTrams: function () {
+      return this.info.tramStops && this.info.tramStops.length > 0
+    },
+    typeCount: function() {
+      var count = 0;
+      if (this.gotBusses()) ++count;
+      if (this.gotTrains()) ++count;
+      if (this.gotMetros()) ++count;
+      if (this.gotTrams()) ++count;
+      return count;
     }
   }
-
 }
 </script>
